ae07052c295e9f83917cda7cbe6aa04bb38facbc
Carlos Augusto Malucelli <carlos.malucelli@preventsenior.com.br>
2020-10-30 11:54:57 -0300

    Added new struct

--- check/check.go
+++ check/check.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -16,18 +17,42 @@ type domain struct {
 	Status []string `json:"status"`
 }
 
+type domainStatus struct {
+	Name       string
+	Expiration time.Time
+	Status     string
+}
+
 // Check - Check expiration date
 func Check(domainURL, option string) {
 
-	var domainInfo domain
+	var info domain
+	var status domainStatus
 
 	resp, err := http.Get("https://rdap.registro.br/domain/" + domainURL)
 
 	if err != nil {
-		panic("Can't create new request.")
+		panic("RDAP error.")
 	}
 
-	_ = json.NewDecoder(resp.Body).Decode(&domainInfo)
+	_ = json.NewDecoder(resp.Body).Decode(&info)
 
-	fmt.Println(domainInfo.Events)
+	for _, event := range info.Events {
+		if event.EventAction == "expiration" {
+			status.Expiration = event.EventDate
+		}
+	}
+
+	status.Name = domainURL
+	status.Status = info.Status[0]
+
+	if option == "status" {
+		fmt.Println(status.Status)
+		os.Exit(0)
+	}
+
+	if option == "expiration" {
+		fmt.Println(status.Expiration)
+		os.Exit(0)
+	}
 }
